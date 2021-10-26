a70dde29d5f80f2055a9a326f4849ae0b94d3e6f
Faisal1999ms <fisal.sy1999@gmail.com>
2021-10-26 00:23:02 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,323 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_1/screen2.dart';
-import 'package:flutter_application_1/screen4.dart';
 import 'package:get/get.dart';
 
 void main() {
-  return (runApp(myapp()));
+  runApp(MYAPP());
 }
 
-class myapp extends StatelessWidget {
+class MYAPP extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: myhomepage(),
-      routes: {
-        '/x4': (context) => screen4(),
-      },
-    );
+  State<StatefulWidget> createState() {
+    return myappless();
   }
 }
 
-class myhomepage extends StatelessWidget {
+class myappless extends State {
   @override
-  Widget build(BuildContext context1) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("screen1"),
-      ),
-      body: Center(
-        child: InkWell(
-          child: Text(
-            "Go to screen2",
-            style: TextStyle(fontSize: 20, color: Colors.blue),
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(0, 116, 176, 1),
+          title: Row(
+            children: [
+              Text(
+                "in ",
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 116, 176, 1),
+                    backgroundColor: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text("LEARNING"),
+              SizedBox(
+                width: 300,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search),
+              )
+            ],
           ),
-          onTap: () => movetoscreen(context1),
+          //leading: Icon(Icons.search),
+        ),
+        body: pages[pagenumeritme],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black54,
+          currentIndex: pagenumeritme,
+          onTap: repag,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: "Topics"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book_sharp), label: "My Learning"),
+          ],
         ),
       ),
     );
   }
-}
 
-void movetoscreen(BuildContext cont) {
-  Navigator.of(cont).push(MaterialPageRoute(builder: (_) {
-    return screen2("info rrom screen1");
-  }));
+  void repag(int ind) {
+    setState(() {
+      pagenumeritme = ind;
+    });
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+          home: Scaffold(
+        appBar: AppBar(
+          title: Text("in LEARNING"),
+          leading: Icon(Icons.search_sharp),
+        ),
+        body: pages[pagenumeritme],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black54,
+          currentIndex: pagenumeritme,
+          onTap: repag,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: "Topics"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book_sharp), label: "My Learning"),
+          ],
+        ),
+      ));
+    }
+  }
+
+  int pagenumeritme = 0;
+//final List<String> item =List.generate(70, (index) => "item:$index");
+  List<Widget> pages = [
+    Scaffold(
+      body: SingleChildScrollView(
+        child: Expanded(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Color.fromRGBO(232, 232, 232, 1),
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color.fromRGBO(254, 254, 254, 1),
+                        ),
+                        margin: EdgeInsets.all(15),
+                        width: 350,
+                        //padding: EdgeInsets.only(right: 22),
+                        alignment: Alignment.center,
+                        child: Image.asset(
+                          "lib/imges/44.png",
+                          fit: BoxFit.fitWidth,
+                        ),
+                      );
+                    },
+                    separatorBuilder: (cont, index) => Divider(),
+                    itemCount: 70),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                width: double.infinity,
+                height: 40,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: Text(
+                  "Top picks for Faisal",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: double.infinity,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: new AssetImage('lib/imges/imag.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.blueGrey,
+                        ),
+                        width: 250,
+                        margin: EdgeInsets.only(right: 22),
+                      );
+                    },
+                    separatorBuilder: (cont, index) => Divider(),
+                    itemCount: 70),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                width: double.infinity,
+                height: 40,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: Text(
+                  "New Releases",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: double.infinity,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: new AssetImage('lib/imges/88.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.blueGrey,
+                        ),
+                        width: 250,
+                        margin: EdgeInsets.only(right: 22),
+                      );
+                    },
+                    separatorBuilder: (cont, index) => Divider(),
+                    itemCount: 70),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                width: double.infinity,
+                height: 40,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: Text(
+                  "Live office hours with experts ",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: double.infinity,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: new AssetImage('lib/imges/77.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.blueGrey,
+                        ),
+                        width: 250,
+                        margin: EdgeInsets.only(right: 22),
+                      );
+                    },
+                    separatorBuilder: (cont, index) => Divider(),
+                    itemCount: 70),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                width: double.infinity,
+                height: 40,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: Text(
+                  "This week's top courses",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: double.infinity,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: new AssetImage('lib/imges/66.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.blueGrey,
+                        ),
+                        width: 250,
+                        margin: EdgeInsets.only(right: 22),
+                      );
+                    },
+                    separatorBuilder: (cont, index) => Divider(),
+                    itemCount: 70),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                width: double.infinity,
+                height: 40,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: Text(
+                  "Popular on LinkedIn learning",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: double.infinity,
+                color: Color.fromRGBO(254, 254, 254, 1),
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: new AssetImage('lib/imges/55.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.blueGrey,
+                        ),
+                        width: 250,
+                        margin: EdgeInsets.only(right: 22),
+                      );
+                    },
+                    separatorBuilder: (cont, index) => Divider(),
+                    itemCount: 70),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    Scaffold(
+      body: Center(
+        child: Text("Page2"),
+      ),
+    ),
+    Scaffold(
+      body: Center(
+        child: Text("Page3"),
+      ),
+    ),
+  ];
 }
--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,42 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-import 'screen3.dart';
-
 class screen2 extends StatelessWidget {
   final String str1;
   screen2(this.str1);
   @override
   Widget build(BuildContext context2) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("screen2"),
-      ),
-      body: Column(
-        children: [
-          Text(
-            str1,
-            style: TextStyle(fontSize: 20, color: Colors.black),
-          ),
-          Center(
-            child: InkWell(
-              child: Text(
-                "Go to screen3",
-                style: TextStyle(fontSize: 20, color: Colors.blue),
-              ),
-              onTap: () {
-                return movetoscreen(context2);
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("screen2"),
+        ),
+        body: Container());
   }
-}
-
-void movetoscreen(BuildContext cont) {
-  Navigator.of(cont).pushReplacement(MaterialPageRoute(builder: (_) {
-    return screen3();
-  }));
 }
